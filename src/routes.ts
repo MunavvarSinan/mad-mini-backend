637694cafbb6337d8615aa6dbcbb227eada50895
MunavvarSinan <munavvarsinan8@gmail.com>
2023-04-15 16:00:29 +0530

    fixed oauth internal server error

--- src/routes.ts
+++ src/routes.ts
@@ -17,7 +17,7 @@ const accessTokenCookieOptions: CookieOptions = {
     sameSite: 'none',
     secure: true,
     path: '/',
-    domain: 'localhost'
+    domain: 'https://mad-mini-backend.onrender.com'
 }
 
 
